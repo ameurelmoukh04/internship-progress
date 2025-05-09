462ccb0829c6303875fe268b38d905137272b82b
ameurelmoukh04 <ameurelmoukh04@gmail.com>
2025-05-09 09:08:13 +0100

    commited

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -5,13 +5,21 @@ use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-class AuthController{
+class AuthController extends AbstractController{
+
+    #[Route('/api/login/template', methods:['GET'])]
+    
+    public function loginTemplate(){
+        return $this->render('login.html.twig');
+    }
+
     #[Route('/api/register',methods:['POST'])]
     public function register(
         Request $request,
